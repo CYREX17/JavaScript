7798d4749c22a11c6c5a03c2c1fc97e1cdc6a1b4
KF <cyrex_ua@yahoo.com>
2020-12-17 20:02:47 +0200

    first commit

---
+++ TEST/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var leftBorderWidth = 1;
+console.log(leftBorderWidth);
+
+var boolean = true;
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+};
+
+console.log(persone.age);
+console.log(persone["name"]);  //same as persone.name
+
+let arr = ['plum.png', 'orange.jpg','apple.bmp',4];
+console.log(arr[3]);
+
+// alert ("Hello world");
+
+// let answer = confirm("Are you here");
+// console.log(answer);
+
+// let answer = prompt("Что там?", "ответ по умолчанию");
+// console.log(typeof(answer));
+
+//console.log(4+"string")
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
